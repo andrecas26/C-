eafe2f0ddedcab08615e3db1bf11d3e096b3ab37
andrecas26 <47376198+andrecas26@users.noreply.github.com>
2019-05-30 23:44:26 -0500

    added procos file

---
+++ ProCos/ProCos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class driverslicense
+    {
+        public string firstName { set; get; }
+        public string lastName { set; get; }
+        public string gender { set; get; }
+        public string licenseNumber { set; get; }
+    public driverslicense (string firstName, string lastName, string gender, string licenseNumber)
+        {
+            
+        }
+    
+    public string getFullName()
+    {
+        return ("firstName" + "lastName");
+    }
+    }
+    
+
+
+    public class book
+    {
+        public string title;
+        public string authors;
+        public int pages;
+        public int Sku;
+        public string publisher;
+        public int price;
+    }
+
+    public class airplane
+    {
+        public string Manufacturer;
+        public string model;
+        public string variant;
+        public int capacity;
+        public int engines;
+    }
+}
